f958d21d5eb8fd5b0c1c5910d15938da346d9244
Mandhoj Thing <np03cs4s220391@heraldcollege.edu.np>
2024-05-24 23:01:08 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Mandhoj";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <img src="" alt="" />
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, aliquam animi culpa, explicabo dolore ea magnam reprehenderit quo adipisci assumenda quasi asperiores illo. Ex reprehenderit deleniti laudantium veniam nostrum eius?
+      </p>
     </div>
+    </>
   );
 }
 
