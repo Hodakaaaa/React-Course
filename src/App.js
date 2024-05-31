fd8bdc60b0a67741994babcc44d6c89fdda05dce
Mandhoj Thing <np03cs4s220391@heraldcollege.edu.np>
2024-05-31 09:36:10 +0545

    Day-16

--- src/App.js
+++ src/App.js
@@ -1,57 +1,53 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Alert from './Components/Alert';
-// import About from './Components/About';
+import About from './Components/About';
+
+// Router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const[mode, setMode] = useState('light');   //Whether dark mode is light or dark
-  const[alert, setAlert] = useState(null);
+  const [mode, setMode] = useState('light'); // Whether dark mode is light or dark
+  const [alert, setAlert] = useState(null);
 
-
-  const showAlert = (message, type) =>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
-    })
-    setTimeout(()=>{
+    });
+    setTimeout(() => {
       setAlert(null);
     }, 1500);
   }
 
-  //creating function
-  const toggleMode = () =>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
-      showAlert("Dark mode has been enabled", "success: ");
+      showAlert("Dark mode has been enabled", "success");
       document.title = "Mandhoj Text Utils | Dark Mode";
-      // setInterval(()=>{
-      //   document.title = "Welcome ";
-      // }, 2000);
-
-      // setInterval(()=>{
-      //   document.title = "Install Netflix";
-      // }, 1500);
-    }else{
+    } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success: ");
+      showAlert("Light mode has been enabled", "success");
       document.title = "Mandhoj Text Utils | Light Mode";
     }
-    
   }
+
   return (
     <>
-    <Navbar title="TextUtils" mode = {mode} toggleMode={toggleMode} About="About-Us"/>
-    <Alert alert={alert}/>
-
-    <div className="container my-3">
-
-      <TextForm showAlert={showAlert} heading="Enter your text to analyze" mode = {mode}/>
-      {/* <About/> */}
-    </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} About="About-Us" />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter your text to analyze" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
